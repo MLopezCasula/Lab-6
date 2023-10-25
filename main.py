e72918f42db835736412d4ab3d88a8ec5f37eaa4
Marcoaurelio Lopez-Casula <marcoaurelio.lopezcasula@gmail.com>
2023-10-25 17:02:29 -0400

    Added Decoder

--- main.py
+++ main.py
@@ -13,3 +13,27 @@ def encode(password):
 if __name__ == "__main__":
     password = input("Please enter your password to encode:")
     encode(password)
+
+    user_choice = 0
+    plain = 0
+    encoded = 0
+    while user_choice != 3:
+        # print menu
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        # get user input
+        user_choice = int(input("Please enter an option: "))
+
+        if user_choice == 1:  # get string and encode it
+            plain = input("Please enter your password to encode: ")
+            encoded = encode(plain)
+            print("Your password has been encoded and stored!")
+        elif user_choice == 2:  # print decoded string
+
+        elif user_choice == 3:  # quit
+            sys.exit()
+
